a35a9640fc9a29b113d20cb1c828da09007b7540
Khafid Cahyanudin <khafidcahya@gmail.com>
2022-09-25 09:16:23 +0700

    ubah semua
    mengurangi yang tidak perlu

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,8 @@
 
   /**
    * Easy selector helper function
-   */
+   * Bagian ini harus tersedia
+  */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -19,9 +20,12 @@
     }
   }
 
+
+
   /**
    * Easy event listener function
-   */
+   * bagian ini harus tersedia
+  */
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
 
@@ -34,28 +38,36 @@
     }
   }
 
+
+
   /**
    * Scrolls to an element with header offset
-   */
+  
   const scrollto = (el) => {
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
     })
   }
+*/
+
 
   /**
    * Mobile nav toggle
-   */
+   * Jika pada mobile maka tanda strip tiga akan muncul dan dapat diklik
+  */
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
+
+
   /**
    * Scrool with ofset on links with a class name .scrollto
-   */
+   * untuk beralih ke abour resume dan portofolio
+  */
   on('click', '#navbar .nav-link', function(e) {
     let section = select(this.hash)
     if (section) {
@@ -107,9 +119,11 @@
     }
   }, true)
 
+
+
   /**
    * Activate/show sections on load with hash links
-   */
+  
   window.addEventListener('load', () => {
     if (window.location.hash) {
       let initial_nav = select(window.location.hash)
@@ -136,10 +150,13 @@
       }
     }
   });
+*/
+
 
   /**
    * Skills animation
-   */
+   * Animasi untuk persentase skill
+  */
   let skilsContent = select('.skills-content');
   if (skilsContent) {
     new Waypoint({
@@ -154,38 +171,11 @@
     })
   }
 
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
 
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
 
   /**
    * Porfolio isotope and filter
-   */
+  */
   window.addEventListener('load', () => {
     let portfolioContainer = select('.portfolio-container');
     if (portfolioContainer) {
@@ -211,25 +201,31 @@
 
   });
 
+
+
   /**
    * Initiate portfolio lightbox 
-   */
+  */
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
 
+
+
   /**
    * Initiate portfolio details lightbox 
-   */
+  
   const portfolioDetailsLightbox = GLightbox({
     selector: '.portfolio-details-lightbox',
     width: '90%',
     height: '90vh'
   });
+*/
+
 
   /**
    * Portfolio details slider
-   */
+  
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
@@ -243,10 +239,13 @@
       clickable: true
     }
   });
+*/
+
 
   /**
    * Initiate Pure Counter 
-   */
+  
   new PureCounter();
+*/
 
 })()
